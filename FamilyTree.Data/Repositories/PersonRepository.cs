b757dac4e378d0356bd2d25feab1f5926dfe7ddb
Rafael Melikyan <melikyanrafael13@gmail.com>
2025-10-07 23:40:16 +0300

    Создал репозиторий для работы с людьми

---
+++ FamilyTree.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FamilyTree.Data.Context;
+
+namespace FamilyTree.Data.Repositories
+{
+    public class PersonRepository(FamilyTreeContext context) : IPersonRepository
+    {
+        private readonly FamilyTreeContext _context = context;
+        public Task AddPersonAsync(Person person)
+        {
+            /*string sql =
+                @"INSERT INTO Person (ID, LastName, FirstName, MiddleName, BirthDate, DeathDate, GenderID, MotherID, FatherID) 
+                VALUES (@ID, @LastName, @FirstName, @MiddleName, @BirthDate, @DeathDate, @GenderID, @MotherID, @FatherID)";
+
+            _context.
+
+            return Task.FromResult(0);*/
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePersonAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> GetAllPersonAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person?> GetPersonByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePersonAsync(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
