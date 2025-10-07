bbc41afed3df15057f285eb44caf433919515190
Rafael Melikyan <melikyanrafael13@gmail.com>
2025-10-07 21:29:10 +0300

    Добавил интерфейс для взаимодействия с моделью человека в приложении

---
+++ FamilyTree.Data/Interfaces/IPersonRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree.Data.Interfaces
+{
+    /// <summary>
+    /// Репозиторий для работы с людьми
+    /// </summary>
+    public interface IPersonRepository
+    {
+        /// <summary>
+        /// Получить всех людей без родителей
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Person>> GetAllPersonAsync();
+        /// <summary>
+        /// Получить человека со ссылками на родителей
+        /// </summary>
+        /// <param name="id">ID человека</param>
+        /// <returns></returns>
+        public Task<Person?> GetPersonByIdAsync(Guid id);
+        /// <summary>
+        /// Добавить нового человека
+        /// </summary>
+        /// <param name="person">Добавляемая информация о человеке</param>
+        /// <returns></returns>
+        public Task AddPersonAsync(Person person);
+        /// <summary>
+        /// Обновление информации о человеке
+        /// </summary>
+        /// <param name="person">Информация о человеке</param>
+        /// <returns></returns>
+        public Task UpdatePersonAsync(Person person);
+        /// <summary>
+        /// Удаление человека по ID
+        /// </summary>
+        /// <param name="id">ID человека</param>
+        /// <returns></returns>
+        public Task DeletePersonAsync(Guid id);
+    }
+}
