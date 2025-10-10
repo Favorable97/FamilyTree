d81251bcf512fbf3fb72a1006b33043c02c994d6
Rafael Melikyan <melikyanrafael13@gmail.com>
2025-10-10 13:22:11 +0300

    Добавил стартовые сервисы

--- FamilyTree.API/ServiceProviderExtensions.cs
+++ FamilyTree.API/ServiceProviderExtensions.cs
@@ -1,4 +1,7 @@
-﻿using FamilyTree.Data.Context;
+﻿
+using FamilyTree.Data.Context;
+using FamilyTree.Data.Interfaces;
+using FamilyTree.Data.Repositories;
 
 namespace FamilyTree.API
 {
@@ -6,7 +9,12 @@ namespace FamilyTree.API
     {
         public static void AddStartServices(this WebApplicationBuilder builder)
         {
+            // Добавляем сервис для подключения к БД через DI
             builder.Services.AddScoped<FamilyTreeContext>(_ => new FamilyTreeContext(builder.Configuration.GetConnectionString("mssql")!));
+
+            builder.Services.AddScoped<IPersonService, PersonService>();
+            
+            builder.Services.AddScoped<IPersonRepository, PersonRepository>();
         }
     }
 }
