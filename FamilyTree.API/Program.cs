11f24de8f4e2e24ba91add4b5dfb8aa32178ebc1
Rafael Melikyan <melikyanrafael13@gmail.com>
2025-10-30 22:39:56 +0300

    Добавил валидацию по типу ответа в роутинг

--- FamilyTree.API/Program.cs
+++ FamilyTree.API/Program.cs
@@ -13,35 +13,35 @@ app.MapGet("/ft/api/persons", async (IPersonService service) =>
 {
     var result = await service.GetAllPersonAsync();
 
-    return Results.Ok(result);
+    return result.Success ? Results.Ok(result) : Results.NotFound(result);
 });
 
 app.MapGet("/ft/api/persons/{id}", async (IPersonService service, Guid id) =>
 {
     var result = await service.GetPersonByIdAsync(id);
 
-    return Results.Ok(result);
+    return result.Success ? Results.Ok(result) : Results.NotFound(result);
 });
 
 app.MapPost("/ft/api/persons", async (IPersonService service, RequestAddPersonDTO data) =>
 {
     var result = await service.AddPersonAsync(data);
 
-    return Results.Ok(result);
+    return result.Success ? Results.Created($"/ft/api/persons/{result.Data!.Id}", result) : Results.BadRequest(result);
 });
 
 app.MapPatch("/ft/api/persons/{id}", async (IPersonService service, Guid id, RequestUpdatePersonDTO data) =>
 {
     var result = await service.UpdatePersonAsync(id, data);
 
-    return Results.Ok(result);
+    return result.Success ? Results.Ok(result) : Results.NotFound(result);
 });
 
 app.MapDelete("/ft/api/persons/{id}", async (IPersonService service, Guid id) =>
 {
     var result = await service.DeletePersonAsync(id);
 
-    return Results.Ok(result);
+    return result.Success ? Results.Ok(result) : Results.BadRequest(result);
 });
 
 app.Run();
