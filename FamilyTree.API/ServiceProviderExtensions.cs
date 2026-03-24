2b6b5a83952e965dee58d29c885bd42d845f2cb7
Rafael Melikyan <melikyanrafael13@gmail.com>
2026-03-24 19:24:29 +0300

    Добавил сериализатор json для вывода строкового значения enum

--- FamilyTree.API/ServiceProviderExtensions.cs
+++ FamilyTree.API/ServiceProviderExtensions.cs
@@ -5,6 +5,7 @@ using FamilyTree.Data.Interfaces;
 using FamilyTree.Data.Repositories;
 using FluentValidation;
 using FamilyTree.API.Services;
+using System.Text.Json.Serialization;
 
 namespace FamilyTree.API
 {
@@ -12,6 +13,11 @@ namespace FamilyTree.API
     {
         public static void AddStartServices(this WebApplicationBuilder builder)
         {
+            builder.Services.ConfigureHttpJsonOptions(options =>
+            {
+                options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
             // Добавляем сервис для подключения к БД через DI
             builder.Services.AddScoped<FamilyTreeContext>(_ => new FamilyTreeContext(builder.Configuration.GetConnectionString("mssql")!));
 
@@ -20,6 +26,14 @@ namespace FamilyTree.API
             builder.Services.AddScoped<IFamilyTreeService, FamilyTreeService>();
             
             builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+
+            builder.Services.AddScoped<IMarriageService, MarriageService>();
+
+            builder.Services.AddScoped<IMarriageRepository, MarriageRepository>();
+
+            builder.Services.AddScoped<ILifeEventService, LifeEventService>();
+
+            builder.Services.AddScoped<ILifeEventRepository, LifeEventRepository>();
 
             // Достаточно объявления только этой строки, чтобы взялись все валидаторы из этой сборки, которые наследуются от AbstractValidator
             builder.Services.AddValidatorsFromAssemblyContaining<CreatePersonValidator>();
